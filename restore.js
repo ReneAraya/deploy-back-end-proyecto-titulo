03c9deeb908f009675bba73c5a182e0db919d823
René Araya <renepatricioarayarojas@gmail.com>
2025-10-20 19:29:55 -0300

    nomenclartura arreglada

--- restore.js
+++ restore.js
@@ -4,7 +4,7 @@ const pool = require('./db');
 
 (async () => {
   try {
-    const sql = fs.readFileSync('./db/web_app_database2_plain.sql', 'utf8');
+    const sql = fs.readFileSync('./DB/web_app_database2_plain.sql', 'utf8');
     console.log('⏳ Restaurando base de datos...');
     await pool.query(sql);
     console.log('✅ Base de datos restaurada correctamente.');
